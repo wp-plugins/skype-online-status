b8e452b24703443596b0633da7dca4289bd506fe
RavanH <RavanH@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-08 15:57:24 +0000

    commit ja.js + dk.js
    git-svn-id: https://plugins.svn.wordpress.org/skype-online-status/trunk@286858 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/langs/da.js
@@ -0,0 +1,13 @@
+﻿// US lang variables
+// For Wordpress > 2.5x
+if ( tinyMCE.addI18n ){
+	tinyMCE.addI18n('en.sosquicktag',{
+		title : 'Tilføj et lyn-tag - quicktag - for Skype Online Status',
+	});
+}
+else
+{
+	tinyMCE.addToLang('sosquicktag',{
+		title : 'Tilføj et lyn-tag - quicktag - for Skype Online Status',
+	});
+}
---
+++ js/langs/ja.js
@@ -0,0 +1,13 @@
+// JA lang variables
+// Wordpress > 2.5x対応
+if ( tinyMCE.addI18n ){
+	tinyMCE.addI18n('en.sosquicktag',{
+		タイトル: 'スカイプオンラインステータスクイックタグを追加',
+	});
+}
+else
+{
+	tinyMCE.addToLang('sosquicktag',{
+		タイトル: 'スカイプオンラインステータスクイックタグを追加',
+	});
+}
