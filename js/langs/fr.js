7223c0e7082876b238150412c77ae3b9f5a48511
RavanH <RavanH@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-30 18:49:42 +0000

    adding french translation, removing all .po files
    git-svn-id: https://plugins.svn.wordpress.org/skype-online-status/tags/2.8.2@317093 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/langs/fr.js
@@ -0,0 +1,13 @@
+// FR lang variables
+// For Wordpress > 2.5x
+if ( tinyMCE.addI18n ){
+	tinyMCE.addI18n('fr.sosquicktag',{
+		title : 'Ajouter un quicktag Skype Online Status',
+	});
+}
+else
+{
+	tinyMCE.addToLang('sosquicktag',{
+		title : 'Ajouter un quicktag Skype Online Status',
+	});
+}
