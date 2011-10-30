d6d2480c1a63157254761899918973fc90df6911
RavanH <RavanH@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-10-30 17:48:06 +0000

    oops, forgot these
    git-svn-id: https://plugins.svn.wordpress.org/skype-online-status/tags/2.8.2@457422 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/langs/fi.js
@@ -0,0 +1,13 @@
+// US lang variables
+// For Wordpress > 2.5x
+if ( tinyMCE.addI18n ){
+	tinyMCE.addI18n('en.sosquicktag',{
+		title : 'Lis‰‰ Skype Online Status -painike',
+	});
+}
+else
+{
+	tinyMCE.addToLang('sosquicktag',{
+		title : 'Lis‰‰ Skype Online Status -painike',
+	});
+}
