b436c2a612d73c85dcfd4b3ab65ff66210580bbf
RavanH <RavanH@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-23 12:19:23 +0000

    bulgarian translation
    git-svn-id: https://plugins.svn.wordpress.org/skype-online-status/tags/2.8.4@548013 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/langs/bg.js
@@ -0,0 +1,13 @@
+// US lang variables
+// For Wordpress > 2.5x
+if ( tinyMCE.addI18n ){
+	tinyMCE.addI18n('en.sosquicktag',{
+		title : 'Добави Skype Online Status бърз-таг',
+	});
+}
+else
+{
+	tinyMCE.addToLang('sosquicktag',{
+		title : 'Добави Skype Online Status бърз-таг',
+	});
+}
